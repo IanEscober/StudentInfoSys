a20982cb4ff05eb2e709bbba95c675bed4bb79d8
Gabriel Ian E. Abe <gabel@you-source.com>
2019-08-23 09:15:57 +0800

    Added enrollments controller

---
+++ src/StudentInfoSys.Application/Controllers/EnrollmentsController.cs
@@ -0,0 +1,48 @@
+﻿namespace StudentInfoSys.Application.Controllers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using StudentInfoSys.Application.Models.ViewModels;
+    using StudentInfoSys.Domain.Interfaces.Services;
+
+    [Authorize]
+    [ApiController]
+    [Route("api/students")]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(string), StatusCodes.Status401Unauthorized)]
+    public class EnrollmentsController : ControllerBase
+    {
+        private readonly IEnrollmentService enrollmentService;
+
+        public EnrollmentsController(IEnrollmentService enrollmentService)
+        {
+            this.enrollmentService = enrollmentService;
+        }
+
+        [HttpPost("{id}/enrollments")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> Post(int id, [FromBody] CourseViewModel course)
+        {
+            var enrollment = await this.enrollmentService.AddCourseToStudentAsync(id, course.CourseId);
+
+            if (enrollment != null)
+            {
+                return this.Ok("Successfully Enrolled");
+            }
+
+            return this.BadRequest(new { message = "The student or course does not exists" });
+        }
+
+        [HttpDelete("{id}/enrollments")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        public async Task<ActionResult> Delete(int id, [FromBody] CourseViewModel course)
+        {
+            await this.enrollmentService.RemoveCourseFromStudentAsync(id, course.CourseId);
+
+            return this.Ok("Successfully Removed");
+        }
+    }
+}
---
+++ tests/StudentInfoSys.Application.Tests/Controllers/EnrollmentsControllerTests.cs
@@ -0,0 +1,59 @@
+﻿namespace StudentInfoSys.Application.Tests.Controllers
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Moq;
+    using StudentInfoSys.Application.Controllers;
+    using StudentInfoSys.Application.Models.ViewModels;
+    using StudentInfoSys.Domain.Entities;
+    using StudentInfoSys.Domain.Interfaces.Services;
+    using Xunit;
+
+    public class EnrollmentsControllerTests
+    {
+        [Fact]
+        public async Task PostByIdAndCourse_WithValidModel_ShouldReturnOk()
+        {
+            var mockService = new Mock<IEnrollmentService>();
+            mockService.Setup(service => service.AddCourseToStudentAsync(0, 0))
+                .ReturnsAsync(new Enrollment())
+                .Verifiable();
+            var controller = new EnrollmentsController(mockService.Object);
+
+            var response = await controller.Post(0, new CourseViewModel());
+
+            Assert.IsType<OkObjectResult>(response);
+            mockService.Verify();
+        }
+
+        [Fact]
+        public async Task PostByIdAndCourse_WithInvalidModel_ShouldReturnBadResponse()
+        {
+            var mockService = new Mock<IEnrollmentService>();
+            mockService.Setup(service => service.AddCourseToStudentAsync(0, 0))
+                .ReturnsAsync(null as Enrollment)
+                .Verifiable();
+            var controller = new EnrollmentsController(mockService.Object);
+
+            var response = await controller.Post(0, new CourseViewModel());
+
+            Assert.IsType<BadRequestObjectResult>(response);
+            mockService.Verify();
+        }
+
+        [Fact]
+        public async Task DeleteByIdAndCourse_WithValidModel_ShouldReturnOk()
+        {
+            var mockService = new Mock<IEnrollmentService>();
+            mockService.Setup(service => service.RemoveCourseFromStudentAsync(0, 0))
+                .Returns(Task.CompletedTask)
+                .Verifiable();
+            var controller = new EnrollmentsController(mockService.Object);
+
+            var response = await controller.Delete(0, new CourseViewModel());
+
+            Assert.IsType<OkObjectResult>(response);
+            mockService.Verify();
+        }
+    }
+}
